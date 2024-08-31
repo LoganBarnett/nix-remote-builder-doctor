465d1ecc5aa4468fbeffa23159a2a3aef6b91d3a
Logan Barnett <logustus@gmail.com>
2024-08-31 14:23:29 -0700

    add descriptions to arguments

--- src/cli.rs
+++ src/cli.rs
@@ -6,9 +6,20 @@ use clap::Parser;
   about = "Diagnose Nix remote builder issues.",
 )]
 pub struct Cli {
-  #[arg(env, short, long, default_value = "/etc/nix/machines")]
+  #[arg(
+    env,
+    short,
+    long,
+    default_value = "/etc/nix/machines",
+    help = "Specify the path of the machines file.",
+  )]
   pub machines_file: String,
-  #[arg(env, short, long)]
+  #[arg(
+    env,
+    short,
+    long,
+    help = "Exclude hosts containing the provided string.  Specify multiple times for additional excludes.",
+  )]
   pub exclude: Vec<String>,
   #[command(flatten)]
   pub verbosity: clap_verbosity_flag::Verbosity,
