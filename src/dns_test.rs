488f9f7b0d004e2f3668da7ba795b0ebcf0a2840
Logan Barnett <logustus@gmail.com>
2024-12-12 12:35:23 -0800

    make the DNS test actually work

--- src/dns_test.rs
+++ src/dns_test.rs
@@ -12,21 +12,36 @@ pub struct DnsTest {
 
 impl Test for DnsTest {
   fn test(&self, context: &MachineTestContext) -> Result<TestResult, AppError> {
-    debug!("Resolving host \"{}\"...", context.machine.url.to_string());
-    Ok(
-      match resolved_host(&context.machine.url.to_string()) {
-        Ok(_) => TestResult::Pass(PassData {
+    match context.machine.url.host_str() {
+      Some(host_str) => {
+        debug!("Resolving host \"{}\"...", host_str);
+        Ok(
+          match resolved_host(&host_str) {
+            Ok(_) => TestResult::Pass(PassData {
+              context: context.clone(),
+              test_name: "DNS".into(),
+            }),
+            Err(_) => TestResult::Fail(FailData {
+              context: context.clone(),
+              reason: "Reason not yet supported for this error.".into(),
+              suggestion: "See if another host on the network can resolve this \
+                           host, or check the host directly.".into(),
+              test_name: "DNS".into(),
+            }),
+          }
+        )
+      },
+      None => {
+        Ok(TestResult::Fail(FailData {
           context: context.clone(),
+          reason: format!(
+            "Could not get host name from URL {}",
+            context.machine.url.to_string(),
+          ),
+          suggestion: "Check the URL to make sure it is correct.".into(),
           test_name: "DNS".into(),
-        }),
-        Err(_) => TestResult::Fail(FailData {
-          context: context.clone(),
-          reason: "Reason not supported for this error.".into(),
-          suggestion: "See if another host on the network can resolve this host, \
-                       or check the host directly.".into(),
-          test_name: "DNS".into(),
-        }),
+        }))
       }
-    )
+    }
   }
 }
